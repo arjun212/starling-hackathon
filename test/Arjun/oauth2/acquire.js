9cf9738ebe4e241de5813836935b58505ea52882
Archunan Muhunthan <archunan.muhunthan12@imperial.ac.uk>
2017-03-26 21:29:19 +0100

    removed old files

--- test/Arjun/oauth2/acquire.js
+++
@@ -1,9 +0,0 @@
-var request = require( 'request'     ) ;
-var qs      = require( 'querystring' ) ;
-var http    = require( 'http'        ) ;
-var fs      = require( 'fs'          ) ;
-var open    = require( 'open'        ) ;
-
-
-
-console.log("Hello");
--- test/Arjun/oauth2/server.js
+++ test/Arjun/oauth2/server.js
@@ -6,3 +6,4 @@ var open    = require( 'open'        ) ;
 
 
 
+console.log("Hello There") ;
