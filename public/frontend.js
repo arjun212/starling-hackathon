0197cd5e3fa6f244d5c3b6e0dbe313990e2c93f4
Sam Franklin <samuelhfranklin@gmail.com>
2017-04-08 12:12:17 +0100

    Browserify fix

--- public/frontend.js
+++ public/frontend.js
@@ -14,21 +14,21 @@ app.controller('appController', function appController($scope) {
           date: '8th April',
           merchant: 'Sainsbury',
           receipts: "False",
-          transactionid: "1",
+          id: "1",
           value: "2.99"
         },
         {
           date: '8th April',
           merchant: 'Waitrose',
           receipts: "True",
-          transactionid: "2",
+          id: "2",
           value: "12.99"
         },
         {
           date: '7th April',
           merchant: 'Tesco',
           receipts: "False",
-          transactionid: "3",
+          id: "3",
           value: "17.99"
         },
     ];
