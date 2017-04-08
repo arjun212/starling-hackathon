2bab54efb785d78b8da2022c9e37f0eb885b399e
Archunan Muhunthan <archunan.muhunthan12@imperial.ac.uk>
2017-04-08 10:13:43 +0100

    file retructing

--- api/api.js
+++ api/api.js
@@ -3,15 +3,21 @@ var request = require( 'request' ) ;
 
 var app = express() ;
  
-app.get('/api/oauth', authCallback) ;
 
-app.get('/api/test', testCallback) ;
 
-app.get('/api/random', randomString) ;
+api.get('api/', fnCbk) ;
+
 
 app.listen(8000)
 
 
+
+//=============================================================================
+// Monzo OAuth Logic, If we need it, we'll use it
+//=============================================================================
+
+/*
+app.get('/api/oauth', authCallback) ;
 
 var client_id = 'oauthclient_00009Ila3HV0mDr9V0TEBd' ;
 var client_secret = 'XzLnJsrFUxtYSzlOkIrOcCO9P817PMCiYYGwWtIFXC+lkzyKfTyzW7iZv/ma9JyxSbB9WmPlpZ3QPu6Cqt6n' ;
@@ -19,52 +25,38 @@ var redirect_uri = 'http://127.0.0.1:8000/api/oauth' ;
 
 var url = 'https://api.getmondo.co.uk/oauth2/token' ;
 
-function testCallback(req, res) {
-    console.log(req);
-}
-
-function randomString(req, res) {
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var stringLength = 5;
-
-    var randomString = Array.apply(null, new Array(stringLength)).map(function () {
-        return possible[Math.floor(Math.random() * possible.length)];
-    }).join('');
-
-    console.log(randomString);
-    return randomString;
-}
 
 function authCallback(req, res) 
 {
 
-var oauth = {
- 'grant_type'    : req.query.grant_type || 'authorization_code' ,
- 'client_id'     : client_id      ,
- 'client_secret' : client_secret  ,
- 'redirect_uri'  : redirect_uri   ,
- [req.query.grant_type === 'refresh_token' ? 'refresh_token'         : 'code']        :
-  req.query.grant_type === 'refresh_token' ? req.query.refresh_token : req.query.code
-}
-
-console.log(oauth);
-
-request.post( 
-	{ 
-		'url'   : url   ,
-		'form' : oauth
-	}, 
-	(err, response, body) => 
-	{
-	    if (!err && response.statusCode === 200) 
-	    {
-	      res.status(200).json( JSON.parse( body ) ) ;
-	    } 
-	    else 
-	    {
-	      res.status(response.statusCode).json( { message: body } ) ; 
-	    }
+	var oauth = {
+	 'grant_type'    : req.query.grant_type || 'authorization_code' ,
+	 'client_id'     : client_id      ,
+	 'client_secret' : client_secret  ,
+	 'redirect_uri'  : redirect_uri   ,
+	 [req.query.grant_type === 'refresh_token' ? 'refresh_token'         : 'code']        :
+	  req.query.grant_type === 'refresh_token' ? req.query.refresh_token : req.query.code
 	}
-) ;
+	
+	console.log(oauth);
+	
+	request.post( 
+		{ 
+			'url'   : url   ,
+			'form' : oauth
+		}, 
+		(err, response, body) => 
+		{
+		    if (!err && response.statusCode === 200) 
+		    {
+		      res.status(200).json( JSON.parse( body ) ) ;
+		    } 
+		    else 
+		    {
+		      res.status(response.statusCode).json( { message: body } ) ; 
+		    }
+		}
+	) ;
 
 }
+*/
