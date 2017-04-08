7712bd2688a1b2e6e59c25eb2dc0b5ce04f8e4e4
Archunan Muhunthan <archunan.muhunthan12@imperial.ac.uk>
2017-04-08 21:49:24 +0100

    non https

--- public/frontend.js
+++ public/frontend.js
@@ -15,31 +15,31 @@ app.controller('appController', function appController($scope, $http) {
         oneAtATime: true
       };
 
-    $http.get("https://mas-starling-hackathon-rest.herokuapp.com/api/getAllTransactions")
+    $http.get('http://mas-starling-hackathon-rest.herokuapp.com/api/getAllTransactions')
     .then(function(response) {
         $scope.transactions = response.data;
     });
 
     $scope.items = [
         {
-          transactionid: "5df5d3df-e9d6-4543-9b98-c475bd4f8c9f",
-          name: "Spinach",
-          value: "15.45"
+          transactionid: '5df5d3df-e9d6-4543-9b98-c475bd4f8c9f',
+          name: 'Spinach',
+          value: '15.45'
         },
         {
-          transactionid: "5df5d3df-e9d6-4543-9b98-c475bd4f8c9f",
-          name: "Tropicana Orange Juice",
-          value: "5.45"
+          transactionid: '5df5d3df-e9d6-4543-9b98-c475bd4f8c9f',
+          name: 'Tropicana Orange Juice',
+          value: '5.45'
         },
         {
-          transactionid: "5df5d3df-e9d6-4543-9b98-c475bd4f8c9f",
-          name: "Bananas",
-          value: "3.45"
+          transactionid: '5df5d3df-e9d6-4543-9b98-c475bd4f8c9f',
+          name: 'Bananas',
+          value: '3.45'
         },
         {
-          transactionid: "fb3951f7-6961-496b-a4d5-158cc079ae70",
-          name: "Bananas",
-          value: "3.75"
+          transactionid: 'fb3951f7-6961-496b-a4d5-158cc079ae70',
+          name: 'Bananas',
+          value: '3.75'
         }
     ];
 
