1be59f151126a20c726333962b99dc2ef60c3851
Mohit Kulkarni <mohit.kulkarni12@imperial.ac.uk>
2017-04-09 01:58:15 +0100

    Added breakdown page

--- ionic-app/src/pages/home/home.ts
+++ ionic-app/src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http} from "@angular/http";
+import {Component, NgZone, ViewChild} from "@angular/core";
+import * as io from 'socket.io-client';
+import {SocketService} from '../../providers/socket-service/socket-service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  messages: any;
+    socketHost: string;
+    zone: NgZone;
+    chatBox: string;
+    json: string;
 
-  }
+    constructor(http: Http, public socket: SocketService) {
+        this.messages = [];
+        this.socketHost = "http://localhost:3000";
+        this.zone = new NgZone({enableLongStackTrace: false});
+       
+        this.chatBox = "";
+        this.json = "";
+        this.socket.socketService.subscribe(event => {
+        console.log('message received from server... ', event);
+        if (event.category === 'message'){
+          this.zone.run(() => {
+            this.messages.push(event.message);
+          });
+        }
+      }); //end of subscribe
 
+    }
+
+
+    // send(message) {
+    //     let newMsg = this.formatMessage(message);
+    //     if (newMsg) {
+    //       // let newMsg = this.formatMessage(msg);
+    //       this.socket.sendMessage(newMsg);
+    //       console.log('emitting: ', newMsg);
+    //     }
+    //     this.json = '';
+    // }
+}
 }
